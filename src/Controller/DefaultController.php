c5b9d540bdf61b0d8a909beca710a434f211d191
PETER HARMAN - U451183 <peter.harman@ext.inetpsa.com>
2018-03-01 10:21:05 +0100

    Initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route(path="/", name="home_page")
+     *
+     * @param Request $request
+     */
+    public function indexAction(Request $request) {
+        return $this->render('default/default.html.twig');
+    }
+}
